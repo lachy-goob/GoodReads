159faacab4c6ea2d0f49192e52a17c9c6a1dbe59
Lachlan <lachlangoebel@gmail.com>
2025-04-12 22:07:32 +1000

    init project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GoodReads
+
 A lightweight chrome extension that populates a list to display in your library
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Popup from "./components/Popup";
+
+const App: React.FC = () => {
+  return <Popup />;
+};
+
+export default App;
